d982304d87b57949f044fc8bc15627dae9dd5bd2
Nicolae Nicoleta Antonia <82367099+Antonia000@users.noreply.github.com>
2021-08-13 15:09:20 +0300

    Login and Sign Up Pages

---
+++ app.js
@@ -0,0 +1,42 @@
+
+$(function () {
+
+    $(".fieldone").click(function () {
+        $("#username").addClass("up");
+        $(".fieldone i").addClass("up");
+    })
+
+    $(".fieldtwo").click(function () {
+        $("#pswd").addClass("up");
+        $(".fieldtwo i").addClass("up");
+    })
+
+
+    $.ajax({
+        type: 'GET',
+        url: "http://jsonplaceholder.typicode.com/users",
+        success: function (data) {
+            $users = $(".response");
+    
+            $("#login").click(function () {
+                let ok = false;
+                for (let i = 0; i < data.length; i++) {
+                    
+                    if ( $("#usr").val() === data[i].username ) {
+                        console.log(data);
+                        $users.text('Logged in succesfully!');
+                        //plus verificare parola
+                        ok = true;
+                    }
+
+                    if (ok == false) $users.text('Account not found!');
+                }
+
+               
+             })
+        }
+    })
+
+
+
+})
